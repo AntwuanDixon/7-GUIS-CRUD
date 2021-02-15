9a601639883d06742272e6927fdb81011baf99a0
AntwuanDixon <evanwchipman@gmail.com>
2021-02-15 18:51:05 +0700

    add error message if no entry is selected

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const firstNameInput = document.querySelector('#first-name');
 const lastNameInput = document.querySelector('#last-name');
 const rolodexList = document.querySelector('#rolodex-list')
 const filter = document.querySelector('.filter')
+const messageContainer =  document.querySelector('.message-container')
 
 var rolodex = [];
 
@@ -25,17 +26,19 @@ function Entry () {
 }
 
 function addToRolodex (person) {
+    console.log('cycle')
     rolodex.push(person);
     rolodex.sort(function (a, b) {
-        if (a.lastName <= b.lastName) {
-            if (a.firstName < b.firstName) {
-                return -1;
-            } else {
-                return 1;
-            }
+        if (a.lastName < b.lastName) {
+            return -1;
         } else if (a.lastName > b.lastName) {
             return 1;
         } else {
+            if (a.firstName < b.firstName) {
+                return -1;
+            } else if (a.firstName > b.firstName) {
+                return 1;
+            }
             return 0;
         }
     })
@@ -71,6 +74,10 @@ function appendPeople (person, index) {
     listItem.addEventListener('click', function () {
         lastNameInput.value = person.lastName;
         firstNameInput.value = person.firstName;
+        const errorMessages = document.querySelectorAll('.error-message')
+        errorMessages.forEach(function(message) {
+            message.remove();
+        })
     })
 }
 
@@ -81,29 +88,48 @@ function removeFromRolodex () {
 }
 
 deleteBtn.addEventListener('click', function () {
-    removeFromRolodex();
-    var selectedPerson = rolodexList.selectedOptions[0];
-    rolodexList.removeChild(selectedPerson);
+    if (rolodexList.selectedOptions[0] !== undefined) {
+        removeFromRolodex();
+        var selectedPerson = rolodexList.selectedOptions[0];
+        rolodexList.removeChild(selectedPerson);
+    } else {
+        displayErrorMessage();
+    }
 })
 
 updateBtn.addEventListener('click', function () {
-    var selectedPerson = parseInt(rolodexList.selectedOptions[0].dataset.person);
-    rolodex.splice(selectedPerson, 1);
-    Entry();
-    clearInputs();
+    if (rolodexList.selectedOptions[0] !== undefined) {
+        var selectedPerson = parseInt(rolodexList.selectedOptions[0].dataset.person);
+        rolodex.splice(selectedPerson, 1);
+        Entry();
+        clearInputs();
+    } else {
+        displayErrorMessage();
+    }
 })
 
 filter.addEventListener('input', function () {
     filterEntries()
 })
 
+function displayErrorMessage () {
+    const errorMessages = document.querySelectorAll('.error-message')
+    errorMessages.forEach(function(message) {
+        message.remove();
+    })
+    var errorMessage = document.createElement('p');
+    errorMessage.innerHTML = "Please select an entry.";
+    errorMessage.classList = "error-message";
+    messageContainer.appendChild(errorMessage);
+}
+
 function filterEntries () {
     clearRolodex();
     rolodex.forEach(function(entry, index) {
-        var fullName = entry.lastName + ', ' + entry.firstName;
-        var filterCaps = filt.value.toLowerCase();
-        
-        if (fullName.includes(filter.value)) {
+        var filterNoCaps = filter.value.toLowerCase();
+        var entryNoCaps = entry.lastName.toLowerCase();
+
+        if (entryNoCaps.startsWith(filterNoCaps)) {
             appendPeople(entry, index)
         }
     })
